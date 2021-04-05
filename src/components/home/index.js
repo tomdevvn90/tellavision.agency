e6bd90101eeb1899e038cc8a39fd3a6bc8cb3968
Hieuhuynh <conghieuhuynh91@gmail.com>
2021-04-05 14:26:55 +0700

    Update animate

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -135,8 +135,8 @@ function HomePage(props) {
       title.style.setProperty( 'left', `${ menuItemPos.x - mainPos.x }px` )
       title.style.setProperty( 'font-size', '30px' )
       title.style.setProperty( 'font-family', 'Playfair Display SC' )
-      title.style.setProperty( 'transition', '1.2s' )
-      title.style.setProperty( '-webkit-transition', '1.2s' )
+      title.style.setProperty( 'transition', '1.4s' )
+      title.style.setProperty( '-webkit-transition', '1.4s' )
       title.innerHTML = menu.title
       document.querySelector( '#main' ).appendChild( title )
 
