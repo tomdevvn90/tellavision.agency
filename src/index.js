7ebaf3f86e3de51fe6127aabd9f463c54075b1b2
discussionforall <discussionforall@gmail.com>
2021-05-11 17:34:46 +0530

    Added 4 new fonts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
 
 import './index.css';
+import './assets/fonts/fonts.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store/store'
